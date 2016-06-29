ecd9a2821d361b88a7fe1746cfe72b2208da83ce
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-29 03:42:32 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         Charcoal
 // @namespace    http://iap-reloaded.github.io/Charcoal
-// @version      0.4
+// @version      0.5
 // @description  dark themes
 // @author       You
 // @match        https://www.google.com/
-// @grant        none
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @grant   GM_getValue
+// @grant   GM_setValue
+// @grant   GM_addStyle
 // ==/UserScript==
     
     if (window.location.hostname  == "www.google.com")  {
-    var sheet = document.createElement('style');
-    sheet.innerHTML = ".lga {background-color: black;}";
-    document.body.appendChild(sheet);
+    GM_addStyle('.lga {background-color: black;}');
         }
