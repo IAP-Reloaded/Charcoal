b12bad76d013c8cd9c6ad43d2d258bf53b0d081d
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-29 03:43:12 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Charcoal
 // @namespace    http://iap-reloaded.github.io/Charcoal
-// @version      0.5
+// @version      0.6
 // @description  dark themes
 // @author       You
 // @match        https://www.google.com/
@@ -12,5 +12,5 @@
 // ==/UserScript==
     
     if (window.location.hostname  == "www.google.com")  {
-    GM_addStyle('.lga {background-color: black;}');
+    GM_addStyle('#lga {background-color: black;}');
         }
